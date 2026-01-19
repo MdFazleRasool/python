f1cdd65b348bcd3d54728bd5cec6d6aa2e9fcde8
MdFazleRasool <fazleamity001@gmail.com>
2026-01-19 19:21:14 +0530

    Data types

--- 001Basic/variables.py
+++ 001Basic/variables.py
@@ -13,12 +13,44 @@ exam_date = '12-09-25'  # snake case
 
 #DataTypes
 
+#int
 a=-34
+
+#float
 b=56.4
 c=12/4 # why this always be false
 print(type (a))
 print(type (b))
 print(type (c))
 
-complex_data_type = 30j
+#complex
+complex_data_type = 30j # to make a variable of complex data type add j
 print(type (complex_data_type))
+
+#String
+s='abc' # signle quote declaration
+t="abc" # double quote declaration
+print(s+" " + t)
+
+
+#boolean
+b=True
+print(type(b))
+t = False
+print(type(t))
+
+print(ord('a')) # ord() ->   char -> unicode
+print(chr(97))  # ch() -> unicode -> char 
+
+
+#String 
+"""
+Indexing in String starts from 
+
+# left to right =  0 -> (length-1)
+
+# right to left  =  -1 -> -(length)
+"""
+s="MFRasool"
+print("positive indexing"+s[0])
+print("negative indexing"+s[-8])
