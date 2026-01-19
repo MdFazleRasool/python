2f0d6790e8d7bfe031ec82ceb6ad8d6a46f135ce
MdFazleRasool <fazleamity001@gmail.com>
2026-01-19 18:56:33 +0530

    Naming Convention in Python

---
+++ 001Basic/variables.py
@@ -0,0 +1,12 @@
+"""
+
+You can not use numbers at variable start
+You can not use spaces in variables.
+ You should not use special characters in variables.
+"""
+
+#Naming Conventions
+
+examDate = '12-09-25' # camel case
+ExamDate = '12-09-25'  # Pascal case
+exam_date = '12-09-25'  # snake case
