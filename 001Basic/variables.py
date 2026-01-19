589a1f763576e9c1d9e613803b2f27fef51cc4ba
MdFazleRasool <fazleamity001@gmail.com>
2026-01-19 18:58:57 +0530

    Data Types in Python

--- 001Basic/variables.py
+++ 001Basic/variables.py
@@ -10,3 +10,15 @@ You can not use spaces in variables.
 examDate = '12-09-25' # camel case
 ExamDate = '12-09-25'  # Pascal case
 exam_date = '12-09-25'  # snake case
+
+#DataTypes
+
+a=-34
+b=56.4
+c=12/4 # why this always be false
+print(type (a))
+print(type (b))
+print(type (c))
+
+complex_data_type = 30j
+print(type (complex_data_type))
